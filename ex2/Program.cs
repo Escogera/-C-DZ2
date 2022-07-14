0097b17dc0b0b03191e7088abaf20964348a3e8e
Vladimir <maostyle@mail.ru>
2022-07-14 21:52:11 +0300

    add ex2 not final

---
+++ ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6*/
+
+
+Console.WriteLine("Введите число");
+int[] numbers = new int [2];
+int size = numbers.Length;
+if(numbers.Length<3)
+{
+    Console.WriteLine("третий цифры нет");
+}
+else
+{
+    Console.WriteLine(numbers[2]);
+}
